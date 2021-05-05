d9ee8f02fff61175896b2b922f0cc4324e4b44e3
wallisp <wallis.phillip@gmail.com>
2021-05-05 12:31:22 -0700

    bug fix in test

--- tests/SCRATCH_test_lora_gpt2.py
+++ tests/SCRATCH_test_lora_gpt2.py
@@ -8,11 +8,16 @@ tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
 # instantiate pretrained model with lora params
 model = GPT2LMHeadModel.from_pretrained('gpt2')
+print('CHECKING FOR LoRA MODEL PARAMS')
 for n, p in model.named_parameters():
-	print(n)
+    if 'adapter' in n:
+	    print(n, p.shape)
 
-text = "this is a test"
+text = 'this is a test'
+
+print('testing model forward...')
+print(f'input text: {text}')
 input_tensor = tokenizer.encode(text, return_tensors='pt')
 outputs = model(input_tensor)
-print(outputs.logits.shape)
+print(f'logits output shape (should be [1, seq_len, vocab_size]): {outputs.logits.shape}')
 
